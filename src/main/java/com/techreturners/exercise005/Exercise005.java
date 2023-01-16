0f6acd19f0dfc538b3aa574cb477ae45a26a6ce2
irinapodoprigora <ukrira@gmail.com>
2023-01-16 20:22:57 +0000

    The Exercise005 was completed

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,26 @@
 package com.techreturners.exercise005;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        if (input == null) {
+            throw new IllegalArgumentException("Input is null");
+        }
+        if (!input.codePoints().allMatch(c -> c < 128)) {
+            throw new IllegalArgumentException("Input contains non ASCII symbol");
+        }
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+
+            char currentChar = input.charAt(i);
+            if (currentChar >= 'a' && currentChar <= 'z' || currentChar >= 'A' && currentChar <= 'Z') {
+                set.add(currentChar);
+            }
+        }
+        return set.size() == 26;
     }
 
 }
--- src/test/java/com/techreturners/exercise005/Exercise005Test.java
+++ src/test/java/com/techreturners/exercise005/Exercise005Test.java
@@ -1,17 +1,15 @@
 package com.techreturners.exercise005;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Exercise005Test {
 
     private Exercise005 ex005;
 
-    @BeforeAll
+    @BeforeEach
     public void setup() {
         ex005 = new Exercise005();
     }
@@ -21,52 +19,57 @@ public class Exercise005Test {
         assertFalse(ex005.isPangram(""));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkPerfectLowerCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("abcdefghijklmnopqrstuvwxyz"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkPerfectUpperCaseSentenceIsAPangram() {
         assertTrue(ex005.isPangram("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("the quick brown fox jumps over the lazy dog"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithMixedUpperAndLowerCaseIsAPangram() {
         assertTrue(ex005.isPangram("The Five boXing wiZards Jump QuicklY"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterXIsNotAPangram() {
         assertFalse(ex005.isPangram("the quick brown fo jumps over the lazy dog"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithMissingLetterHIsNotAPangram() {
         assertFalse(ex005.isPangram("five boxing wizards jump quickly at it"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithUnderscoresIsAPangram() {
         assertTrue(ex005.isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog"));
     }
 
-    @Disabled("You can remove this @Disabled annotation to run the test")
     @Test
     public void checkSentenceWithNumbersIsAPangram() {
-        assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs"));
+        assertTrue(ex005.isPangram("the 1 quick brown fox jumps over the 2 lazy dogs "));
+    }
+
+    @Test
+    void checkThrowExceptionIfSentenceHasNonASCII() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ex005.isPangram("¤EÀsÆW°ê»Ú®i¶T¤¤¤ß3¼Ó®i¶TÆU2~~KITEC 3/F Rotunda 2" ));
+        String expected = "Input contains non ASCII symbol";
+        assertEquals(expected, exception.getMessage());
+    }
+    @Test
+    void checkThrowExceptionIfSentenceIsNull() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ex005.isPangram( null ));
+        String expected = "Input is null";
+        assertEquals(expected, exception.getMessage());
     }
 
 }
